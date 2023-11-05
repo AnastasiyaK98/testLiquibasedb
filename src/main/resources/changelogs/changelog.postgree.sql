4781fe51e2c919c2d740dd2a0f221fa545a0798c
Анастасия <123438765+AnastasiyaK98@users.noreply.github.com>
2023-11-06 03:59:28 +0500

    Update changelog.postgree.sql

--- src/main/resources/changelogs/changelog.postgree.sql
+++ src/main/resources/changelogs/changelog.postgree.sql
@@ -2,7 +2,7 @@
 
 --changeset your.name:1
 -- comment: create person table
---rollback DROP TABLE person;
+
 create table person (
     id int primary key,
     name varchar(50) not null,
@@ -10,8 +10,9 @@ create table person (
     address2 varchar(50),
     city varchar(30)
 )
+--rollback DROP TABLE person;
 --changeset your.name:2
 -- comment: insert person table
---rollback DELETE FROM person WHERE name='Sofa';
 INSERT INTO person (id, name, address1, address2, city) VALUES (1, 'Sofa', 'Чусовой 4', 'Пермь', 'Киров');
+--rollback DELETE FROM person WHERE name='Sofa';
 
