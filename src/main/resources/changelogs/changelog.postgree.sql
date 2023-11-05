497d6fe4486bc7256718ff93bef0840e9b6a6db0
Анастасия <123438765+AnastasiyaK98@users.noreply.github.com>
2023-11-05 19:23:37 +0500

    Update changelog.postgree.sql

--- src/main/resources/changelogs/changelog.postgree.sql
+++ src/main/resources/changelogs/changelog.postgree.sql
@@ -1,6 +1,7 @@
 --liquibase formatted sql
 
 --changeset your.name:1
+-- comment: create person table
 --rollback DROP TABLE person;
 create table person (
     id int primary key,
@@ -10,6 +11,7 @@ create table person (
     city varchar(30)
 )
 --changeset your.name:2
+-- comment: insert person table
 --rollback DELETE FROM person WHERE name='Sofa';
 INSERT INTO person (id, name, address1, address2, city) VALUES (1, 'Sofa', 'Чусовой 4', 'Пермь', 'Киров');
 
